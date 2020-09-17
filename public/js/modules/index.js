e6629f00080bfb079c81b4610636b4a19a86e396
Your Name <you@example.com>
2020-09-17 05:04:25 +0200

    added scroll snap

--- public/js/modules/index.js
+++ public/js/modules/index.js
@@ -1,46 +1,19 @@
-// export const index = document.title = 'Beyond Brilliance';
-// // export const about = document.title = 'Multidisciplinary design & development agency';
-// // export const contact = document.title = 'Contacts | Selta Digital Studio';
+/*
 
+* Selta Digital Studio Scripts 
 
-export function draw(e) 
-{ 
+*/
+
+function _(id) { return document.getElementById(id); }
+
+export function draw(e) { 
     e.preventDefault();
     return (
-        document.querySelector('#drawer').classList.toggle('active')
+        _('drawer').classList.toggle('active'),
+        _('burger').classList.toggle('active')
     ); 
 }
 
-let slideIndex = 1;
-showSlides(slideIndex);
-
-export function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-export function currSec(n) {
-    showSlides(slideIndex = n);
-}
-
-export function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("section");
-    let pointers = document.getElementsByClassName("pointer");
-
-    n > slides.length ? slideIndex = 1 : '';
-    n < 1 ? slideIndex = slides.length : '';
-    
-    for (var value of slides) {
-        value.style.display = 'none';
-    }
-
-    for (var el of pointers) {
-        el.className = el.className.replace(" active", "");
-    }
-
-    slides[slideIndex - 1].style.display = "grid";
-    pointers[slideIndex - 1].className += " active";
-}
 
 
 
